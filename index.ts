5468e843aa1b6c24d16adafecc57b8f670bb486d
romanskym1 <roman@yielder.io>
2024-11-27 17:46:47 +0200

    fix build

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-export * from "./lib";
+// index.ts
+export * from './lib/immutableUtils';
+export * from './lib/switcher';
+export * from './lib/utils';
+export * from './lib/tsUtils';
+export * from './lib/logging';
