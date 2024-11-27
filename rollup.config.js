9c041613edac40fc2a11cf522d7efad04651f739
romanskym1 <roman@yielder.io>
2024-11-27 17:35:39 +0200

    fix build

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,27 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-    input: 'lib/index.ts', // Your library's entry point
-    output: [
-        {file: 'dist/index.js', format: 'cjs', sourcemap: true},
-        {file: 'dist/index.esm.js', format: 'esm', sourcemap: true},
-    ],
-    plugins: [typescript(), resolve(), commonjs(),],
+  input: 'index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+    }),
+    resolve(),
+    commonjs(),
+  ],
+  external: ['spark-md5'], // Add your external dependencies here
 };
