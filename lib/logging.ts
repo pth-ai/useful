2fce4636b302d9976cb49036d0dd7aed8610787c
romanskym1 <roman@yielder.io>
2024-12-03 10:18:00 +0200

    improve logger with useful file logging utility

--- lib/logging.ts
+++ lib/logging.ts
@@ -1,10 +1,39 @@
+import {hasOwnProperty} from "./tsUtils";
+import * as uuid from "uuid";
+import path from "path";
+import {switcher} from "./switcher";
+
+interface LogEntry {
+    level: 'info' | 'debug' | 'error' | 'warm' | string;
+    message: string;
+
+    [optionName: string]: any;
+}
+
+type CTLogger = Logger & { getLastId: () => string };
+
 export interface Logger {
     debug: (message: string, meta?: object) => void;
     info: (message: string, meta?: object) => void;
+    warn: (message: string, meta?: object) => void;
     error: (message: string, error: any, meta?: object) => void;
+    log: (entry: LogEntry) => void;
+    getLastId: () => string;
+    setLastId: (lastId: string) => void;
 }
 
-const basicLogger: Logger = {
+let lastId = "";
+const basicLogger: CTLogger = {
+    log(entry: LogEntry): void {
+        switcher<typeof entry.level>()
+            .when('info', () => basicLogger.info(entry.message, entry['meta']))
+            .when('debug', () => basicLogger.debug(entry.message, entry['meta']))
+            .when('error', () => basicLogger.error(entry.message, entry['error'], entry['meta']))
+            .when('warm', () => basicLogger.warn(entry.message, entry['meta']))
+            .fallback(() => basicLogger.info(entry.message, entry['meta']))
+            .checkExhaustive()
+            .exec(entry.level);
+    },
     debug(message: string, meta?: object): void {
         console.debug("[DEBUG]", message, meta)
     },
@@ -13,6 +42,15 @@ const basicLogger: Logger = {
     },
     info(message: string, meta?: object): void {
         console.info("[INFO]", message, meta);
+    },
+    warn(message: string, meta?: object): void {
+        console.info("[WARN]", message, meta);
+    },
+    setLastId(lastIdToSet: string): void {
+        lastId = lastIdToSet;
+    },
+    getLastId(): string {
+        return lastId;
     }
 };
 
@@ -25,26 +63,102 @@ export const setLogger = (newLogger: Logger) => {
 };
 
 // Export a logger that delegates to the current global logger.
-export const logger: Logger = {
-    debug(message: string, meta?: object): void {
-        globalLogger.debug(message, meta);
-    },
-    error(message: string, error: any, meta?: object): void {
-        globalLogger.error(message, error, meta);
-    },
-    info(message: string, meta?: object): void {
-        globalLogger.info(message, meta);
-    }
-};
+export const logger: Logger = globalLogger;
 
-export const prefixedLogger = (prefix: string): Logger => ({
-    debug(message: string, meta?: object): void {
-        globalLogger.debug(`[${prefix}] ${message}`, meta);
-    },
-    error(message: string, error: any, meta?: object): void {
-        globalLogger.error(`[${prefix}] ${message}`, error, meta);
-    },
-    info(message: string, meta?: object): void {
-        globalLogger.info(`[${prefix}] ${message}`, meta);
+const serverId = uuid.v4().substring(0, 5);
+
+export const createLoggerForFile = () => {
+    // Capture the Error stack
+    const err = new Error();
+    const stack = err.stack || '';
+
+    // Split the stack into lines and find the caller
+    const stackLines = stack.split('\n');
+    let callerFileName = '';
+
+    // Look for the first occurrence in the stack where a file in this project is mentioned
+    for (let i = 2; i < stackLines.length; i++) {
+        if (stackLines[i].indexOf(__filename) === -1) {
+            // Extract file name using regular expression
+            const matches = /at\s+(?:.*?\s+)?(?:\(?(.*?):\d+:\d+\)?)/.exec(stackLines[i]);
+            if (matches && matches[1]) {
+                callerFileName = matches[1];
+                break;
+            }
+        }
     }
-});
+
+    // Return the filename without its extension
+    const filename = path.basename(callerFileName, path.extname(callerFileName));
+    return new Logging(filename);
+}
+
+
+export class Logging {
+    component: string;
+    appVersion: string;
+    private baseMeta: {};
+
+    constructor(component: string, baseMeta: {} = {}, private logger: Logger = globalLogger) {
+        this.component = component;
+        this.appVersion = process.env.APP_VERSION || 'dev';
+        this.baseMeta = baseMeta;
+    }
+
+    private doLog = (logOpts: LogEntry & { meta: object }): CTLogger => {
+        const logId = uuid.v4().substring(0, 5);
+        const enhandedMeta = {...logOpts.meta, logId};
+        this.logger.log({
+            ...logOpts,
+            meta: enhandedMeta,
+        })
+        this.logger.setLastId(logId);
+
+        return this.logger;
+    }
+
+    public info = (message: string, meta: object = {}) =>
+        logger.log({
+            level: 'info',
+            message,
+            meta: this.genMeta(meta),
+        })
+
+    public debug = (message: string, meta: object = {}, isEnabled: boolean = false) =>
+        isEnabled && logger.log({
+            level: 'debug',
+            message,
+            meta: this.genMeta(meta)
+        })
+
+
+    public error = (message: string, error: any, meta: object = {}): CTLogger =>
+        this.doLog({
+            level: 'error',
+            message: message + (hasOwnProperty(error, 'message') ? ` [${error.message}]` : ''),
+            meta: this.genMeta({
+                error: {
+                    code: error.code,
+                    url: error.url,
+                    stack: error.stack
+                },
+                ...meta
+            })
+        })
+
+    public warn = (message: string, meta: object = {}) =>
+        logger.log({
+            level: 'warn',
+            message,
+            meta: this.genMeta(meta)
+        })
+
+    private genMeta = (obj: object) => ({
+        serverId,
+        component: this.component,
+        appVersion: this.appVersion,
+        ...this.baseMeta,
+        ...obj,
+    })
+
+}
